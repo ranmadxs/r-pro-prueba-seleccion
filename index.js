20c3b78b390f5d4e29a9bfb66a990f55b6ee848a
Edgar Sanchez <esanchezdeg@sodimac.cl>
2020-02-01 20:14:57 -0300

    agragando pregunta01

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ app.options('*', cors());
 
 app.get('/', (req, res) => res.send('Prueba de Selección Subgerencia Desarrollo Tecnológico Gerencia de Sistemas Retail Omnicanal'));
 
+app.get('/pregunta01/:palabra', function (req, res) {
+   res.writeHead(200, {'Content-Type': 'application/json'});
+   var response = { "response" : "This is GET method with palabra=" + req.params.palabra + "." }
+   res.end(JSON.stringify(response));
+})
+
 app.listen(process.env.PORT || 3000, function() {
     console.log('server running on port 3000', '');
 });
